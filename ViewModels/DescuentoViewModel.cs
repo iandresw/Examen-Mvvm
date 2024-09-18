e749f17e061e82da3c6bf17f47b03b49531566ce
iandresw <rbejarano@unah.hn>
2024-09-17 22:25:18 -0600

    Create DescuentoViewModel.cs
    se creo la clase view model usando la librería CommunityToolkit.Mvvm.ComponentModel se ageraron 6 variables con [oobservableproperty] y dos [relaycomand] calcular y limpiar

---
+++ ViewModels/DescuentoViewModel.cs
@@ -0,0 +1,53 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Examen_Mvvm.Models;
+
+namespace Examen_Mvvm.ViewModels
+{
+    public partial class DescuentoViewModel: ObservableObject
+    {
+        [ObservableProperty]
+        private decimal producto1;
+
+        [ObservableProperty]
+        private decimal producto2;
+
+        [ObservableProperty]
+        private decimal producto3;
+
+        [ObservableProperty]
+        private decimal subtotal;
+
+        [ObservableProperty]
+        private decimal descuento;
+
+        [ObservableProperty]
+        private decimal total;
+
+        [RelayCommand]
+        public void Calcular()
+        {
+            var model = new DescuentoModel
+            {
+                Producto1 = Producto1,
+                Producto2 = Producto2,
+                Producto3 = Producto3
+            };
+
+            Subtotal = model.Subtotal;
+            Descuento = model.Descuento;
+            Total = model.Total;
+        }
+        [RelayCommand]
+        public void Limpiar()
+        {
+            Producto1 = 0;
+            Producto2 = 0;
+            Producto3 = 0;
+            Subtotal = 0;
+            Descuento = 0;
+            Total = 0;
+        }
+    }
+
+}
